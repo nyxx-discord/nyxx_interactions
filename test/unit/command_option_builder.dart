eca1618d4e1ce8381b2e9733b392a158bde70d77
Szymon Uglis <szymon@l7ssha.xyz>
2021-10-30 18:38:13 +0200

    Implement new unit tests

---
+++ test/unit/command_option_builder.dart
@@ -0,0 +1,54 @@
+import 'package:nyxx/nyxx.dart';
+import 'package:nyxx_interactions/nyxx_interactions.dart';
+import 'package:test/test.dart';
+
+main() {
+  test('.registerHandler success', () {
+    final builder = CommandOptionBuilder(CommandOptionType.subCommand, 'test', 'test');
+
+    builder.registerHandler((p0) async => null);
+    expect(builder.handler, isA<SlashCommandHandler>());
+  }, skip: 'Currently broken due bug in nyxx');
+
+
+  test('.registerHandler failure', () {
+    final builder = CommandOptionBuilder(CommandOptionType.user, 'test', 'test');
+
+    expect(() => builder.registerHandler((p0) async => null), throwsA(isA<StateError>()));
+  });
+
+  test('.build', () {
+    final builder = CommandOptionBuilder(
+        CommandOptionType.channel,
+        'test',
+        'test',
+        choices:  [
+          ArgChoiceBuilder("arg1", "val1"),
+        ],
+        channelTypes:  [
+          ChannelType.text,
+        ],
+        autoComplete: true
+    );
+
+    final expectedResult = {
+      "type": CommandOptionType.channel.value,
+      "name": "test",
+      'description': 'test',
+      'default': false,
+      'required': false,
+      'choices': [
+        {
+          'name': 'arg1',
+          'value': 'val1'
+        }
+      ],
+      'channel_types': [
+        ChannelType.text
+      ],
+      'autocomplete': true
+    };
+
+    expect(builder.build(), equals(expectedResult));
+  });
+}
---
+++ test/unit/slash_command_builder.dart
@@ -0,0 +1,66 @@
+import 'package:nyxx/nyxx.dart';
+import "package:nyxx_interactions/nyxx_interactions.dart";
+
+import "package:test/test.dart";
+
+void main() {
+  test("invalid name", () {
+    expect(() => SlashCommandBuilder("invalid name", "test", []), throwsA(isA<ArgumentError>()));
+  });
+
+  test("missing description", () {
+    expect(() => SlashCommandBuilder("invalid-name", null, []), throwsA(isA<ArgumentError>()));
+  });
+
+  test("description present for context menu", () {
+    expect(() => SlashCommandBuilder("invalid-name", "test", [], type: SlashCommandType.user), throwsA(isA<ArgumentError>()));
+  });
+
+  test(".setId", () {
+    final slashCommandBuilder = SlashCommandBuilder("invalid-name", "test", []);
+
+    expect(() => slashCommandBuilder.id, throwsA(isA<Error>()));
+
+    slashCommandBuilder.setId(Snowflake.zero());
+    expect(slashCommandBuilder.id, equals(Snowflake.zero()));
+  });
+
+  test(".addPermission", () {
+    final slashCommandBuilder = SlashCommandBuilder("invalid-name", "test", []);
+
+    slashCommandBuilder
+      ..addPermission(RoleCommandPermissionBuilder(Snowflake.zero()))
+      ..addPermission(UserCommandPermissionBuilder(Snowflake.bulk()));
+
+    expect(slashCommandBuilder.permissions, isNotNull);
+    expect(slashCommandBuilder.permissions, hasLength(2));
+  });
+
+  test('.registerHandler failure', () {
+    final slashCommandBuilder = SlashCommandBuilder("invalid-name", "test", [
+      CommandOptionBuilder(CommandOptionType.subCommand, "test", 'test')
+    ]);
+
+    expect(() => slashCommandBuilder.registerHandler((p0) async => null), throwsA(isA<ArgumentError>()));
+  });
+
+  test('.registerHandler success', () {
+    final slashCommandBuilder = SlashCommandBuilder("invalid-name", "test", []);
+
+    slashCommandBuilder.registerHandler((p0) async => null);
+    expect(slashCommandBuilder.handler, isA<SlashCommandHandler>());
+  }, skip: 'Currently broken due bug in nyxx');
+
+  test('.build', () {
+    final slashCommandBuilder = SlashCommandBuilder("invalid-name", "test", []);
+
+    final expectedResult = {
+      "name": "invalid-name",
+      "description": "test",
+      "type": SlashCommandType.chat,
+      "default_permission": true,
+    };
+
+    expect(slashCommandBuilder.build(), equals(expectedResult));
+  });
+}
